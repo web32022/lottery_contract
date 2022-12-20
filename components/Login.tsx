834a6b1d83d028e1d76a4985e25494697f86563c
web32022 <121041666+web32022@users.noreply.github.com>
2022-12-20 16:46:46 +0100

    Add files via upload

--- components/Login.tsx
+++ components/Login.tsx
@@ -13,7 +13,7 @@ function Login() {
              src="https://i.imgur.com/4h7mAu7.png"/>
              <h1 className='text-2xl text-white font-bold'>The WealthGamers</h1>
              <h2 className='text-white text-1xl'>Africa's First Decentralized Lucky Wheel Platform </h2>
-             <p className='text-white text-xs text-center mt-3 font-extrabold'>Our Platform Is Totally Free From Human Interaction And Runs Decentralized On The Ethereum Blockchain & Smart Contracts</p>
+             <p className='text-white text-xs text-center mt-3 font-extrabold'>Our Platform Is Totally Free From Human Interaction And Runs Decentralized On The Blockchain</p>
              <button onClick={connectwithMetamask} 
              className='bg-white px-8 py-5 mt-10 rounded-lg font-bold shadow-lg'>Login With Metamask</button>
               <p className='text-white text-center text-large font-extrabold mt-6 animate-pulse'>Powered By ASpaceLife Technologies</p>
